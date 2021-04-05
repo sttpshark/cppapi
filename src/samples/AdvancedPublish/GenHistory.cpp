fd69421832bdbb9460619ca2135ade27d1f8e717
coryharrington <cory.harrington@uconn.edu>
2021-04-05 04:20:40 -0400

    never added this, need this for clean commit and build

--- src/samples/AdvancedPublish/GenHistory.cpp
+++ src/samples/AdvancedPublish/GenHistory.cpp
@@ -1,7 +1,7 @@
 //******************************************************************************************************
 //  GenHistory.cpp - Gbtc
 //
-//  Copyright © 2019, Grid Protection Alliance.  All Rights Reserved.
+//  Copyright ï¿½ 2019, Grid Protection Alliance.  All Rights Reserved.
 //
 //  Licensed to the Grid Protection Alliance (GPA) under one or more contributor license agreements. See
 //  the NOTICE file distributed with this work for additional information regarding copyright ownership.
@@ -50,7 +50,7 @@ void GenHistory::StartArchive()
     m_history->AddOrUpdateTable(history);
 
     m_subscriber->RegisterNewMeasurementsCallback(&GenHistory::ProcessMeasurements);
-    m_subscriber->Connect("localhost", m_port);
+    m_subscriber->Connect("localhost", m_port, "");
 
     SubscriptionInfo info;
     info.FilterExpression = SubscriberInstance::SubscribeAllNoStatsExpression;
